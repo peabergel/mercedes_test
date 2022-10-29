455cb47e17b49a2d7a24e64b016bbfec4a4a4efe
Pierre-Elie Abergel <pe.abergel@gmail.com>
2022-10-29 12:27:41 +0200

    Push

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,25 @@
 class PagesController < ApplicationController
+  require 'open-uri'
+
   def home
+  end
+
+  def answer
+    @city = answer_params["city"]
+    @query = answer_params["query"]
+    coordinates = Geocoder.coordinates(@city)
+    mapbox_key = "pk.eyJ1IjoiZWxlbmRpbGwiLCJhIjoiY2tvMnZkMmplMG1hajJvbHl0bjh1ZXBhayJ9.o-vbFb1OhCH322s-HLd1lg"
+    lat = coordinates[0]
+    lng = coordinates[1]
+    url = "https://api.mapbox.com/geocoding/v5/mapbox.places/#{@query}.json?limit=5&proximity=#{lng}%2C#{lat}&types=poi&access_token=#{mapbox_key}"
+    @answer_serialized = URI.open(url).read
+    @answers = JSON.parse(@answer_serialized)
+    render json: @answers
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:city, :query)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+  get '/answer', to: 'pages#answer'
+ 
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
