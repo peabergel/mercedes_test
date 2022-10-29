98339b932248f6bfabc32e75f3ba3dc1a548fe4f
Pierre-Elie Abergel <pe.abergel@gmail.com>
2022-10-29 18:58:57 +0200

    Geocoding

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,16 +5,29 @@ class PagesController < ApplicationController
   end
 
   def answer
+    @hash = {}
+    answers_json.each do |answer|
+      postal = answer['context'].find { |element| element['id'].include?("postcode") }
+      if postal.nil?
+        @hash["error"] = "postal code not found"
+      else
+        postal = postal["text"]
+        @hash.key?(postal) ? @hash[postal] << answer['text'] : @hash[postal] = [answer['text']]
+      end
+    end
+    render json: @hash
+  end
+
+  def answers_json
     @city = answer_params["city"]
     @query = answer_params["query"]
-    coordinates = Geocoder.coordinates(@city)
-    mapbox_key = "pk.eyJ1IjoiZWxlbmRpbGwiLCJhIjoiY2tvMnZkMmplMG1hajJvbHl0bjh1ZXBhayJ9.o-vbFb1OhCH322s-HLd1lg"
-    lat = coordinates[0]
-    lng = coordinates[1]
-    url = "https://api.mapbox.com/geocoding/v5/mapbox.places/#{@query}.json?limit=5&proximity=#{lng}%2C#{lat}&types=poi&access_token=#{mapbox_key}"
+    raise
+    coord = Geocoder.coordinates(@city)
+    lat = coord[0]
+    lng = coord[1]
+    url = "https://api.mapbox.com/geocoding/v5/mapbox.places/#{@query}.json?limit=10&proximity=#{lng}%2C#{lat}&types=poi&access_token=#{ENV['MAPBOX_API_KEY']}"
     @answer_serialized = URI.open(url).read
-    @answers = JSON.parse(@answer_serialized)
-    render json: @answers
+    JSON.parse(@answer_serialized)["features"]
   end
 
   private
